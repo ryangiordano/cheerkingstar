b829a57f8089fecd7acd6ada86d3dc762ba337c6
Ryan Giordano <rgiorda1@gmail.com>
2016-02-15 22:03:57 -0500

    more google maps stuff...

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -1,16 +1,26 @@
 app.controller('MapController',['$scope', function($scope){
+  function initMap() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
 
-  var cities = [
-      {
-          city : 'Cheer King Star',
+    $scope.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  }
+  var cheerKing={
+          name : 'Cheer King Star',
           desc : 'Chinese Restaurant in Louisville, KY',
           lat : 38.2545715,
           long : -85.7605157
-
-      }
-  ];
+        }
   var mapOptions = {
-      zoom: 20,
+      zoom:10,
       center: new google.maps.LatLng(38.2545715, -85.7605157),
       mapTypeId: google.maps.MapTypeId.ROADMAPs
   }
@@ -28,25 +38,19 @@ app.controller('MapController',['$scope', function($scope){
           position: new google.maps.LatLng(info.lat, info.long),
           title: info.city
       });
-      marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
 
-      google.maps.event.addListener(marker, 'click', function(){
-          infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-          infoWindow.open($scope.map, marker);
-      });
 
       $scope.markers.push(marker);
 
   }
 
-  for (i = 0; i < cities.length; i++){
-      createMarker(cities[i]);
-  }
 
-  $scope.openInfoWindow = function(e, selectedMarker){
-      e.preventDefault();
-      google.maps.event.trigger(selectedMarker, 'click');
-  }
+  createMarker(cheerKing);
+  $('#mapModal').on('shown.bs.modal', function () {
 
+      google.maps.event.trigger(map, "resize");
+      $scope.map.panTo(marker.getPosition());
+      $scope.map.setZoom(14);
+  });
 
 }]);
