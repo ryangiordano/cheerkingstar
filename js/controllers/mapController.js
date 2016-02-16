18cac9991fb4c8d5923bbee42897290493d87f0a
Ryan Giordano <rgiorda1@gmail.com>
2016-02-15 22:32:33 -0500

    more google maps stuff...

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -1,56 +1,20 @@
 app.controller('MapController',['$scope', function($scope){
   function initMap() {
-    var myLatLng = {lat: -25.363, lng: 131.044};
+    var mapController = this;
+    var myLatLng = {lat: 38.2545715, lng: -85.7605157};
 
     $scope.map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 10,
       center: myLatLng
     });
 
-    var marker = new google.maps.Marker({
+    $scope.marker = new google.maps.Marker({
       position: myLatLng,
-      map: map,
-      title: 'Hello World!'
+      map: $scope.map,
+      title: 'Cheer King Star!'
     });
   }
-  var cheerKing={
-          name : 'Cheer King Star',
-          desc : 'Chinese Restaurant in Louisville, KY',
-          lat : 38.2545715,
-          long : -85.7605157
-        }
-  var mapOptions = {
-      zoom:10,
-      center: new google.maps.LatLng(38.2545715, -85.7605157),
-      mapTypeId: google.maps.MapTypeId.ROADMAPs
-  }
 
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  $scope.markers = [];
-
-  var infoWindow = new google.maps.InfoWindow();
-
-  var createMarker = function (info){
-
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          position: new google.maps.LatLng(info.lat, info.long),
-          title: info.city
-      });
-
-
-      $scope.markers.push(marker);
-
-  }
-
-
-  createMarker(cheerKing);
-  $('#mapModal').on('shown.bs.modal', function () {
-
-      google.maps.event.trigger(map, "resize");
-      $scope.map.panTo(marker.getPosition());
-      $scope.map.setZoom(14);
-  });
+  initMap();
 
 }]);
